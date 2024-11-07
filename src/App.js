f8bc0cbfbdd374df2f99ad3b979a3932ccace1e8
Lucas Borgesgit <lucasborgesmoraes@gmail.com>
2024-11-07 03:17:34 -0300

    moving parts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
-import logo from './assets/tag.png';
+import logo from './assets/5.png';
 import moldura from './assets/moldura.png';
+import basnky from './assets/banksy-disarm-cke.webp';
+import wall from './assets/wall.jpg';
+import tag1 from './assets/smiley.webp';
+import tag2 from './assets/guilty.webp';
 import scrolldown from './assets/scroll.gif';
 
 import './App.css';
 import Tilt from 'react-parallax-tilt';
 import { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 function App() {
+
+  gsap.registerPlugin(ScrollTrigger);
+  useEffect(() => {
+
+    setTimeout(() => {
+      let wallsizes = document.querySelector(".wall");
+      let wallSize = {
+        width: wallsizes.clientWidth,
+        height: wallsizes.clientHeight,
+      };
+
+      console.log(wallSize);
+
+      const tlBasnky = gsap.timeline({
+        scrollTrigger: {
+          trigger: "#basky",
+          start: "top bottom",
+          end: "bottom+=100 bottom",
+          scrub: true,
+          markers: true,
+        },
+      });
+
+      const stick1 = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".wall",
+          start: "top bottom",
+          end: "bottom bottom",
+          scrub: true,
+          markers: true,
+        },
+      });
+
+      const stick2 = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".wall",
+          start: "top bottom",
+          end: "bottom bottom",
+          scrub: true,
+          markers: true,
+        },
+      });
+
+      tlBasnky.fromTo("#basky", { opacity: 0, x: -100 }, { opacity: 1, x: 0 });
+
+      stick1.fromTo(
+        "#sticky1",
+        { opacity: 0, y: 0 },
+        { opacity: 1, y: -wallSize.height / 1.5 }
+      );
+
+      stick2.fromTo(
+        "#sticky2",
+        { opacity: 0, y: 0 },
+        { opacity: 1, y: -wallSize.height / 1.4 }
+      );
+    }, 300);
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -15,9 +79,7 @@ function App() {
         scrolldownImg.classList.add('fadeout');
       }
     };
-
     window.addEventListener('scroll', handleScroll);
-
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
@@ -45,29 +107,41 @@ function App() {
         <img className="scrolldown" src={scrolldown} alt="" />
       </div>
       <div className="text-container">
-        <h1 className="title">Grafitt(AR)</h1>
+        <h1 className="title">
+          Grafitt(<span className="ar">AR</span>)
+        </h1>
         <p className="description">
           O grafite é uma poderosa forma de luta, resistência e expressão. Ele
           nasce das margens da sociedade como um movimento artístico que dá voz
           àqueles que frequentemente não têm espaço nos meios tradicionais.
         </p>
+        <img id="basky" src={basnky} alt="" />
 
-        <p>
+        <p className="description">
           O grafite transforma a paisagem, expressando temas sociais, culturais
           e políticos, muitas vezes expondo injustiças e desafios que as
           comunidades sentem em silêncio. Outras vezes expressando o seu
           cotidiano, sendo uma declaração de amor ou apenas sua opinião sobre
-          algo supérfulo{" "}
+          algo supérfulo
         </p>
       </div>
+      <div className="flying-imgs">
+        <section>
+          <img  className="wall" src={wall} alt="" />
+          <div className='img-divs'>
+          <img id='sticky1' className="sticker" src={tag1} alt="" />
+          <img id='sticky2' className="sticker" src={tag2} alt="" />
+          </div>
+        </section>
+      </div>
 
-      <div className="text-container-2">
-        <p>
+      <div className="text-container">
+        <p className="description">
           {" "}
           No entanto, há um debate sobre os impactos negativos do grafite em
           espaços privados e na arquitetura urbana.{" "}
         </p>
-        <p>
+        <p className="description">
           {" "}
           Muitos veem o grafite em residências, prédios históricos ou áreas
           comerciais como uma forma de "agressão" visual. A crítica se concentra
@@ -75,7 +149,7 @@ function App() {
           artísticas e, em alguns casos, o grafite pode danificar a estética
           arquitetônica ou gerar custos de limpeza.
         </p>
-        <p>
+        <p className="description">
           {" "}
           contraste entre o impacto positivo de dar voz a uma comunidade e as
           críticas ao dano físico leva à reflexão sobre como equilibrar o
@@ -83,8 +157,8 @@ function App() {
         </p>
       </div>
 
-      <div className="text-container-3">
-        <p>
+      <div className="text-container">
+        <p className="description">
           A realidade aumentada pode ser uma alternativa para explorar o
           grafite. A tecnologia permite que as pessoas vejam e interajam com
           obras de arte digitalmente projetadas em espaços públicos ou privados.
@@ -101,15 +175,12 @@ function App() {
         </p>
         <p> video</p>
 
-        <p>
-        Projetos futuros:
-        </p>
+        <p>Projetos futuros:</p>
 
         <p>Login e usuários</p>
         <p>Upload pelos usuários</p>
         <p>votação para arte da semana</p>
         <p>modelos 3D</p>
-
       </div>
     </div>
   );
